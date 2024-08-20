940008338ad4ed54c684657eeaecc3ede9b35e1d
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-08-20 20:04:15 +0200

    mail not working

--- dadi/script.js
+++ dadi/script.js
@@ -1,1 +1,34 @@
-console.log('ok js')
+/* GIOCO DEI DADI
+
+TO DO LIST:
+1 Generare un numero da 1 a 6 sia per il giocatore che per il computer.
+2 Recupera gli elementi del DOM.
+3 Stabilire chi vince in base al numero più alto.
+4 Dichiarare il vincitore in pagina
+*/ 
+
+//Recupero elementi del DOM
+const displayResult = document.getElementById("game")
+
+//Lancio dei dadi
+
+const playerNumber = Math.floor(Math.random(1) * 6 ) + 1;
+const computerNumber = Math.floor(Math.random(1) * 6 ) + 1;
+console.log('Il tuo punteggio è:', playerNumber)
+console.log('Il punteggio del computer è: ', computerNumber )
+
+//Messaggio di vittoria o sconfitta
+let message = 'Pareggio!'
+
+if (playerNumber > computerNumber){
+message = 'Hai vinto!'
+} else if (playerNumber < computerNumber){
+    message = 'Hai perso!'
+}
+
+console.log(message)
+
+
+//Inserimento nel DOM
+displayResult.outerHTML = 
+`Il tuo risultato è: ${playerNumber} <br> Il risultato del computer è: ${computerNumber} <br> ${message}`
--- mail/script.js
+++ mail/script.js
@@ -1,1 +1,48 @@
-console.log('OK JS')
+/*TODO LIST
+1. Crea un array di email autorizzate all'accesso.
+2. Raccogli la mail inviata dall'utente nel form.
+3. Se la email inserita dall'utente corrisponde a una delle mail nell'array, consentigli l'accesso, altrimenti negaglielo.
+4. Stampa il messaggio di accesso negato o consentito.
+ */ 
+
+//Raccolta Elementi dal DOM 
+const emailForm = document.getElementById('insert')
+const outcomeEmail = document.getElementById('outcome')
+const form = document.getElementById('form')
+
+//Creazione array delle mail autorizzate
+const allowedEmail = ['aceattorney@gmail.com', 'yakuza@libero.it', 'discoelysium@yahoo.it'];
+
+
+
+//Eventi
+form.addEventListener('submit', function (event) {
+event.preventDefault();
+
+//Raccolta email
+const email = emailForm.value.trim()
+console.log(email)
+
+
+//Generazione messaggio giusto
+let accessGranted = false;
+console.log(accessGranted)
+let message = 'Accesso negato'
+
+
+//Controllo se la email è una delle email consentite
+for (let i = 0; i < allowedEmail.length; i++ ) {
+    const mailValidator = allowedEmail[i];
+console.log(mailValidator)
+
+ if(email === mailValidator) {
+    let accessGranted = true
+    let message = 'Accesso consentito'
+ }
+ 
+}
+
+
+
+outcomeEmail.innerText = message 
+})
